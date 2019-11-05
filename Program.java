7d6227428e5fc51b502a6c9e4c74df85d78f84c7
student <student@lab-44-12.cs.put.poznan.pl>
2019-11-05 14:31:48 +0100

    'solvingerrors'

--- Program.java
+++ Program.java
@@ -2,11 +2,13 @@ public class MyClass {
   int x = 5;
 
   public static void main(String[] args) {
-    int y = 5
+    int y = 5;//assigning new variable y
     MyClass myObj = new MyClass();
-    x *= y
+
     System.out.println(myObj.x);
     x = y
+    x *= y;
     System.out.println(myObj.x);
+    x = y;
   }
 }
